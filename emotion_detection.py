2cd6e760856e58d9257c91d7415e5caa55bf8ffc
pm2552 <pedro.morais@posteo.pt>
2025-04-06 19:29:53 +0100

    Initial commit

---
+++ emotion_detection.py
@@ -0,0 +1,17 @@
+import requests
+import json
+
+def emotion_detector(text_to_analyze):
+    print("I'm IN")
+    URL = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
+    header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
+    input_json = { "raw_document": { "text": text_to_analyze } }
+    print("Sending request")
+    response = requests.post(URL, json = input_json, headers=header)
+ 
+    if response.status_code == 200:
+        return response
+    else:
+        return response
+
+
